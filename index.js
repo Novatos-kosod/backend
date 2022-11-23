d8ddaa0a40112f477cae974b239abac4cbbd788f
Zetian612 <jsperdomo361@misena.edu.co>
2022-11-22 20:13:18 -0500

    Agregar variable a puerto

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use('/api', router);
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000 ');
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
     }
 );
